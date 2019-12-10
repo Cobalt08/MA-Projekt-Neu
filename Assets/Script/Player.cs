ccabdaadaf0036df2112269b3903e1992498f48c
Marcel S <132@edle-perlen.de>
2019-12-10 13:25:49 +0100

    First commit

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,192 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+public class Player : MonoBehaviour
+
+{
+
+    [SerializeField] float runSpeed = 3f;
+    [SerializeField] float jumpSpeed = 5f;
+    //[SerializeField] float fallMultiplier = 2.5f;
+    //[SerializeField] float lowJumpMultiplier = 2f;
+    [SerializeField] Vector2 deathKick = new Vector2(25f, 25f);
+    [SerializeField] bool activePlayer = true;
+    public GameObject camera;
+
+
+    //private float timer = 0;
+    //private float timerMax = 0;
+
+
+    // State
+    bool isAlive = true;
+    float gravityScaleAtStart;
+    public bool facingRight = true;
+
+    Rigidbody2D myRigidBody;
+    Animator myAnimator;
+    CapsuleCollider2D myBodyCollider;
+    CircleCollider2D myFeet;
+    Joystick myjoystick;
+    JumpButton myjumpbutton;
+    SwitchPlayerButton switchPlayerButton;
+    AttackButton myattackbutton;
+    GrabButton mygrabbutton;
+    Joystick weaponjoystick;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        facingRight = true;
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myBodyCollider = GetComponent<CapsuleCollider2D>();
+        myFeet = GetComponent<CircleCollider2D>();
+        myjoystick = FindObjectOfType<Joystick>();
+        myjumpbutton = FindObjectOfType<JumpButton>();
+        switchPlayerButton = FindObjectOfType<SwitchPlayerButton>();
+        myattackbutton = FindObjectOfType<AttackButton>();
+        mygrabbutton = FindObjectOfType<GrabButton>();
+        //weaponjoystick = GetComponent<Fixed Joystick 2>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //switch Player for testing only
+        if (switchPlayerButton.Pressed)
+        {
+            activePlayer = !activePlayer;
+        }
+        CameraHandling();
+
+        if (!isAlive) { return; }
+
+        //for offline testing
+        if (activePlayer)
+        {
+            Run();
+            Jump();
+            Die();
+            Attack();
+            Grab();
+        }
+    }
+
+    private void CameraHandling(){
+        if (activePlayer == true) {
+            camera.SetActive(true);
+        }
+        if (activePlayer == false) {
+            camera.SetActive(false);
+        }
+
+    }
+
+
+    private void Run()
+    {
+
+
+        float controlThrow = myjoystick.Horizontal; // value is betweeen -1 to +1
+
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+
+
+        myAnimator.SetFloat("Speed", Mathf.Abs(myRigidBody.velocity.x));
+
+
+        // If the input is moving the player right and the player is facing left...
+        if (controlThrow > 0 && !facingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+        // Otherwise if the input is moving the player left and the player is facing right...
+        else if (controlThrow < 0 && facingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+
+    }
+
+    private void Flip()
+    {
+
+        // Switch the way the player is labelled as facing.
+        facingRight = !facingRight;
+
+        transform.Rotate(0f, 180f, 0f);
+
+    }
+
+    private void Jump()
+    {
+        if (!myFeet.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
+        else
+        {
+            myAnimator.SetBool("IsJumping", false);
+        }
+
+
+
+        if (myjumpbutton.Pressed == true)
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            myRigidBody.velocity += jumpVelocityToAdd;
+            myAnimator.SetBool("IsJumping", true);
+        }
+
+
+        /*if (myRigidBody.velocity.y < 0)
+        {
+            myRigidBody.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (myRigidBody.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            myRigidBody.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+        */
+
+
+    }
+
+    private void Die() {
+        if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Hazard"))) {
+            isAlive = false;
+            myAnimator.SetTrigger("Dying");
+            myRigidBody.velocity = deathKick;
+            FindObjectOfType<GameSession>().ProcessPlayerDeath();
+
+        }
+    }
+
+    private void Attack()
+    {
+
+        if (myattackbutton.Pressed == true)
+        {
+            WeaponScript weapon = GetComponent<WeaponScript>();
+            if (weapon != null)
+            {
+                weapon.Attack();
+            }
+        }
+    }
+
+    private void Grab()
+    {
+    }
+
+}
---
+++ Assets/Scripts/Weapon2Script.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Launch projectile
+/// </summary>
+public class WeaponScript2 : MonoBehaviour
+{
+
+    public Transform shotPrefab;
+
+    public float shootingRate = 0.5f;
+
+    private float shootCooldown;
+
+    private Vector2 dir;
+
+    //private Transform randomObject;
+
+    private double deadZone = 0.2;
+
+    void Start()
+    {
+        shootCooldown = 0f;
+    }
+
+    void Update()
+    {
+        if (shootCooldown > 0)
+        {
+            shootCooldown -= Time.deltaTime;
+        }
+    }
+
+    public void Attack()
+    {
+        if (CanAttack)
+        {
+            shootCooldown = shootingRate;
+
+            var shotTransform = Instantiate(shotPrefab) as Transform;
+
+            shotTransform.position = transform.position;
+
+            //var horiz = Input.GetAxis("Horiz");
+            //var vert = Input.GetAxis("Vert");
+
+            Joystick weaponjoystick = GetComponent<Joystick>();
+            float horiz = weaponjoystick.Horizontal;
+            float vert = weaponjoystick.Vertical;
+
+            Vector2 tmp = new Vector2(horiz, vert);
+            if (tmp.sqrMagnitude > deadZone)
+            {
+                dir = tmp.normalized;
+            }
+            shotTransform.position = dir;
+
+            ProjectileMoveScript move = shotTransform.gameObject.GetComponent<ProjectileMoveScript>();
+            Player player = GetComponent<Player>();
+        }
+    }
+
+    /// <summary>
+    /// Is the weapon ready to create a new projectile?
+    /// </summary>
+    public bool CanAttack
+    {
+        get
+        {
+            return shootCooldown <= 0f;
+        }
+    }
+}
