3d7f7cde34a2746a3c8514ae53819818fa7efa48
Cobalt08 <Marcel_Fabien.Schneider@Student.Reutlingen-University.de>
2020-01-04 17:59:20 +0100

    Created the PressureScript.

---
+++ Assets/Scripts/PressureScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressureScript : MonoBehaviour
+{
+    private bool activated = false;
+    public int mass = 3;
+
+    public Sprite sprite1;
+    public Sprite sprite2;
+
+    private SpriteRenderer spriteRenderer;
+
+    void OnCollisionEnter2D(Collider2D otherCollider)
+    {
+        if (otherCollider.gameObject.GetComponent<Rigidbody2D>().mass >= mass) {
+            this.activated = !this.activated;
+
+            if (spriteRenderer == null)
+            {
+                spriteRenderer = GetComponent<SpriteRenderer>();
+            }
+
+            if (spriteRenderer.sprite == sprite1)
+            {
+                spriteRenderer.sprite = sprite2;
+            }
+        }
+    }
+
+    void OnCollisionExit2D(Collider2D otherCollider)
+    {
+          
+            this.activated = !this.activated;
+
+            if (spriteRenderer == null)
+            {
+                spriteRenderer = GetComponent<SpriteRenderer>();
+            }
+
+            if (spriteRenderer.sprite == sprite2)
+            {
+                spriteRenderer.sprite = sprite1;
+            }
+
+        
+    }
+}
