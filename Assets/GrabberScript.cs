0813f089637cc055222195b2b285758cbfd339ea
MarosiF <49545584+MarosiF@users.noreply.github.com>
2020-01-04 01:02:38 +0100

    Grab
    Grabbin skill and objects

--- Assets/GrabberScript.cs
+++ Assets/GrabberScript.cs
@@ -1,76 +1,74 @@
-﻿using System;
+﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class GrabberScript : MonoBehaviour
+public class grabberscript : MonoBehaviour
 {
-    public bool grabbed;
-    RaycastHit2D hit;
-    public float distance = 2f;
-    public Transform holdpoint;
-    public float throwforce;
-    public LayerMask notgrabbed;
 
-    GrabButton mygrabbutton;
+	public bool grabbed;
+	RaycastHit2D hit;
+	public float distance = 2f;
+	public Transform holdpoint;
+	public float throwforce;
+	public LayerMask notgrabbed;
+	GrabButton mygrabbutton;
 
-    // Use this for initialization
-    void Start()
-    {
-        mygrabbutton = FindObjectOfType<GrabButton>();
+	// Use this for initialization
+	void Start()
+	{
+		mygrabbutton = FindObjectOfType<GrabButton>();
+	}
 
-    }
+	// Update is called once per frame
+	void Update()
+	{
 
-    // Update is called once per frame
-    void Update()
-    {
+		//if (Input.GetKeyDown(KeyCode.B))
+		if (mygrabbutton.Pressed)
+			
+		{
 
-        if (mygrabbutton.Pressed == true)
-            Console.WriteLine("Grab Button pressed");
-        {
-            //garb
-            if (!grabbed)
-            {
-                Physics2D.queriesStartInColliders = false;
+			if (!grabbed)
+			{
+				Physics2D.queriesStartInColliders = false;
 
-                hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, distance);
+				hit = Physics2D.Raycast(transform.position, Vector2.right* transform.localScale.x, distance);
 
-                if (hit.collider != null && hit.collider.tag == "Grabbable")
-                {
-                    grabbed = true;
+				if (hit.collider != null && hit.collider.tag == "Grabbable")
+				{
+					grabbed = true;
 
-                }
-
-
-                //throw
-            }
-            else if (!Physics2D.OverlapPoint(holdpoint.position, notgrabbed))
-            {
-                grabbed = false;
-
-                if (hit.collider.gameObject.GetComponent<Rigidbody2D>() != null)
-                {
-
-                    hit.collider.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(transform.localScale.x, 1) * throwforce;
-                }
+				}
 
 
-                
-            }
+				//grab
+			}
+			else if (!Physics2D.OverlapPoint(holdpoint.position, notgrabbed))
+			{
+				grabbed = false;
+
+				if (hit.collider.gameObject.GetComponent<Rigidbody2D>() != null)
+				{
+
+					hit.collider.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(transform.localScale.x, 1) * throwforce;
+				}
 
 
-        }
-
-        if (grabbed)
-            hit.collider.gameObject.transform.position = holdpoint.position;
+				//throw
+			}
 
 
-    }
+		}
 
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.green;
+		if (grabbed)
+			hit.collider.gameObject.transform.position = holdpoint.position;
 
-        Gizmos.DrawLine(transform.position, transform.position + Vector3.right * transform.localScale.x * distance);
-    }
+
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.green;
+
+		Gizmos.DrawLine(transform.position, transform.position + Vector3.right * transform.localScale.x * distance);
+	}
 }
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -36,7 +36,7 @@ public class Player : MonoBehaviour
     JumpButton myjumpbutton;
     SwitchPlayerButton switchPlayerButton;
     AttackButton myattackbutton;
-    GrabButton mygrabbutton;
+    //GrabButton mygrabbutton;
     Joystick weaponjoystick;
 
 
@@ -53,7 +53,7 @@ public class Player : MonoBehaviour
         myjumpbutton = FindObjectOfType<JumpButton>();
         switchPlayerButton = FindObjectOfType<SwitchPlayerButton>();
         myattackbutton = FindObjectOfType<AttackButton>();
-        mygrabbutton = FindObjectOfType<GrabButton>();
+        //mygrabbutton = FindObjectOfType<GrabButton>();
         weaponjoystick = GameObject.Find("Fixed Joystick 2").GetComponent<Joystick>();
 
     }
@@ -109,13 +109,20 @@ public class Player : MonoBehaviour
         if (controlThrow > 0 && !facingRight)
         {
             // ... flip the player.
-            Flip();
+            //Flip();
+            // Switch the way the player is labelled as facing.
+            facingRight = !facingRight;
+           
+            transform.localScale = new Vector3(this.transform.localScale.x*-1, this.transform.localScale.y, this.transform.localScale.z);
         }
         // Otherwise if the input is moving the player left and the player is facing right...
         else if (controlThrow < 0 && facingRight)
         {
             // ... flip the player.
-            Flip();
+            //Flip();
+            // Switch the way the player is labelled as facing.
+            facingRight = !facingRight;
+            transform.localScale = new Vector3(this.transform.localScale.x * -1, this.transform.localScale.y, this.transform.localScale.z);
         }
 
     }
