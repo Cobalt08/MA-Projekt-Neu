d9f6308555d8df92b351a623c6949e41a663a2ed
MarosiF <49545584+MarosiF@users.noreply.github.com>
2020-01-04 15:00:00 +0100

    Switch ver1 Test

---
+++ Assets/Scripts/SwitchVer1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchVer1 : MonoBehaviour
+{
+    [SerializeField] GameObject switchOn;
+    [SerializeField] GameObject switchOff;
+    public bool isOn = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = switchOff.GetComponent<SpriteRenderer>().sprite;
+    }
+
+    // Update is called once per frame
+    void OnTiggerEnter2D(Collider2D col)
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = switchOn.GetComponent<SpriteRenderer>().sprite;
+
+        isOn = true;
+    }
+}
+
