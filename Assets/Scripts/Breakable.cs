22000e1490941b4f658a43746ce1d8e3b9b15f05
MarosiF <49545584+MarosiF@users.noreply.github.com>
2020-01-05 18:27:36 +0100

    Particles

--- Assets/Scripts/Breakable.cs
+++ Assets/Scripts/Breakable.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class Breakable : MonoBehaviour
 {
+    
+    public float lifeTime;
+    public GameObject explosion;
     void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag.Equals("BigProjectile"))
         {
-            Destroy(this.gameObject);
+            //Destroy(this.gameObject);
+            Invoke("DestroyProjectile", lifeTime);
         }
+    }
+
+    void DestroyProjectile(){
+        
+        Instantiate(explosion, transform.position, Quaternion.identity);
+        Destroy(this.gameObject);
     }
 
 }
