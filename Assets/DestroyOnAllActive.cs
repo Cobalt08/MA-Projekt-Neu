c6a46e3dd9d0b1aad9c39ce5326d0816fb99d11d
TLauxmann <tobias_holger.lauxmann@student.reutlingen-university.de>
2020-01-06 22:37:44 +0100

    changed switch destroy object interaction, script now in object

---
+++ Assets/DestroyOnAllActive.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnAllActive : MonoBehaviour
+{
+    public GameObject explosion;
+
+    public Switch[] switches;
+    private bool allActive = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        allActive = true;
+        foreach(Switch sw in switches)
+        {
+            if (!sw.isActive)
+            {
+                allActive = false;
+            }
+        }
+
+        if (allActive)
+        {
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/Switch.cs
+++ Assets/Scripts/Switch.cs
@@ -13,71 +13,24 @@ public class Switch : MonoBehaviour
 	public bool sticks;
 	public bool isActive = false;
 
-    public GameObject[] destroyObjects;
-    public GameObject[] connected;
-
-    private bool destroyed = false;
-    private bool allPressed = true;
-
     // Use this for initialization
     void Start()
 	{
 		anim = GetComponent<Animator>();
 	}
 
-	// Update is called once per frame
-	void Update()
-	{
-
-	}
-
-
 	void OnTriggerStay2D()
 	{
 		anim.SetBool("goDown", true);
 		isActive = true;
 
-        foreach (GameObject sw in connected)
+        /*foreach (DoorTrigger trigger in doorTrig)
         {
-            if (sw.GetComponent<Switch>() != null)
-            {
-                if (!sw.GetComponent<Switch>().isActive)
-                {
-                    allPressed = false;
-                }
-            }
-            else
-            {
-                allPressed = false;
-            }
+
+            trigger.Toggle(true);
+
         }
-
-        if (allPressed)
-        {
-            allPressed = false;
-            if (!destroyed)
-            {
-                foreach (GameObject obj in destroyObjects)
-                {
-
-                    Destroy(obj);
-                }
-                destroyed = true;
-            }
-
-            /*foreach (DoorTrigger trigger in doorTrig)
-            {
-
-                trigger.Toggle(true);
-
-            }
-            */
-        }
-        else
-        {
-            allPressed = true;
-        }
-
+        */
     }
 
 	void OnTriggerExit2D()
