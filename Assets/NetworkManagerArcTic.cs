8f036dd72d209802a68322e1f7cbbdc393a927bf
TLauxmann <tobias_holger.lauxmann@student.reutlingen-university.de>
2020-01-19 22:59:34 +0100

    start multiplayer

---
+++ Assets/NetworkManagerArcTic.cs
@@ -0,0 +1,40 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NetworkManagerArcTic : NetworkManager
+{
+    public Transform leftRacketSpawn;
+    public Transform rightRacketSpawn;
+    GameObject ball;
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        // add player at correct spawn position
+        Transform start = numPlayers == 0 ? leftRacketSpawn : rightRacketSpawn;
+        GameObject player = Instantiate(playerPrefab, start.position, start.rotation);
+        NetworkServer.AddPlayerForConnection(conn, player);
+
+        // spawn ball if two players
+        if (numPlayers == 2)
+        {
+            GameObject.Find("ChooseMenu").GetComponent<MainMenu>().RpcChangeScene();
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            //ball = Instantiate(spawnPrefabs.Find(prefab => prefab.name == "Ball"));
+            // NetworkServer.Spawn(ball);
+        }
+    }
+
+    public override void OnServerDisconnect(NetworkConnection conn)
+    {
+        // destroy ball
+        //if (ball != null)
+        //    NetworkServer.Destroy(ball);
+
+        // call base functionality (actually destroys the player)
+        base.OnServerDisconnect(conn);
+    }
+
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,20 @@
-﻿using System.Collections;
+﻿using Mirror;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-public class MainMenu : MonoBehaviour
+using UnityEngine.UI;
+using TMPro;
+public class MainMenu : NetworkBehaviour
 {
+
+    private NetworkManager networkManager;
+
+    private void Awake()
+    {
+        networkManager = GameObject.Find("Network").GetComponent<NetworkManager>();
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -15,4 +25,35 @@ public class MainMenu : MonoBehaviour
         Debug.Log("Quit");
         Application.Quit();
     }
+
+    public void CreateServer()
+    {
+        networkManager.StartHost();
+    }
+
+    public void JoinServer()
+    {
+        networkManager.networkAddress = GameObject.Find("IpAdressInput").GetComponent<Text>().text;
+        networkManager.StartClient();
+        //switch to Choose Menu
+    }
+
+    public void StopHost()
+    {
+        networkManager.StopHost();
+    }
+
+    public void SwitchPlayers()
+    {
+        string temp = GameObject.Find("GameOwnerChar").GetComponent<TextMeshProUGUI>().text;
+        GameObject.Find("GameOwnerChar").GetComponent<TextMeshProUGUI>().text = GameObject.Find("JointPlayerChar").GetComponent<TextMeshProUGUI>().text;
+        GameObject.Find("JointPlayerChar").GetComponent<TextMeshProUGUI>().text = temp;
+    }
+
+    [ClientRpc]
+    public void RpcChangeScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
 }
