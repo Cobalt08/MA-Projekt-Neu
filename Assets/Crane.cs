1d65b7edbd46c2b762d8580806d1c9f4a5101780
MarosiF <Fanni_Tamara.Marosi@Student.Reutlingen-University.DE>
2020-01-20 13:15:17 +0100

    Cran Asset, script, Animator

---
+++ Assets/Crane.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crane : MonoBehaviour
+{
+    Animator anim;
+    public bool sticks;
+    public bool isActive = false;
+    public Switch Switch;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Open();
+        Close();
+    }
+
+
+    void Open()
+    {
+        if (Switch.isActive)
+        {
+            anim.SetBool("isOn", true);
+            isActive = true;
+        }
+        /*foreach (DoorTrigger trigger in doorTrig)
+		{
+
+			trigger.Toggle(true);
+
+		}
+		*/
+
+    }
+
+    void Close()
+    {
+        if (sticks)
+            return;
+
+        if (!Switch.isActive)
+        {
+            anim.SetBool("isOn", false);
+            isActive = false;
+        }
+        /*foreach (DoorTrigger trigger in doorTrig)
+		{
+
+			trigger.Toggle(false);
+
+		}*/
+
+
+
+
+
+    }
+}
