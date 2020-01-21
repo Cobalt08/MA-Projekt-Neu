08867f4e429069c5c4d17a9bcf8da9c5ef0e9117
MarosiF <Fanni_Tamara.Marosi@Student.Reutlingen-University.DE>
2020-01-21 11:31:38 +0100

    CranAssets

---
+++ Assets/MoveOnTouch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOnTouch : MonoBehaviour
+{
+    [SerializeField] private Vector3 velocity;
+    private bool moving;
+
+    private void OnCollisonEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Player"){
+            moving = true;
+            collision.collider.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisonExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.collider.transform.SetParent(null);
+            moving = false;
+        }
+    }
+
+    
+    private void FixedUpdate() {
+        if (moving) {
+            transform.position += (velocity * Time.deltaTime);
+        }
+    }
+    
+}
