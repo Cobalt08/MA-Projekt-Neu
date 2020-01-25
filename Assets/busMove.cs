23f8cf4f9ee87c67cee9392f5c78044fadbfb08f
Adrian <abeilharz9@gmail.com>
2020-01-25 18:18:38 +0100

    level4

---
+++ Assets/busMove.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class busMove : MonoBehaviour
+{
+
+    
+    public Switch[] switches;
+
+    private bool allActive = false;
+    private Animation anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim = GetComponent<Animation>();
+        allActive = true;
+        foreach (Switch sw in switches)
+        {
+            if (!sw.isActive)
+            {
+                allActive = false;
+                
+            }
+        }
+
+        if (allActive)
+        {
+            this.startAnimation();
+        }
+        else
+        {
+            this.stopAnimation();
+        }
+
+       
+
+    }
+    void startAnimation()
+    {
+        foreach(AnimationState state in anim)
+        {
+            state.speed = 1F;
+        }   
+    }
+
+    void stopAnimation()
+    {
+        foreach (AnimationState state in anim)
+        {
+            state.speed = 0F;
+        }
+
+    }
+}
---
+++ Assets/elevatorScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class elevatorScript : MonoBehaviour
+{
+
+    public Switch[] switches;
+
+    private bool anyActive = false;
+    private Animation anim;
+    // Start is called before the first frame update
+    void Update()
+    {
+        anim = GetComponent<Animation>();
+        anyActive = false;
+        foreach (Switch sw in switches)
+        {
+            if (sw.isActive)
+            {
+                anyActive = true;
+
+            }
+        }
+
+        if (anyActive)
+        {
+            this.startAnimation();
+        }
+        else
+        {
+            this.stopAnimation();
+        }
+    }
+
+    void startAnimation()
+    {
+        foreach (AnimationState state in anim)
+        {
+            state.speed = 1F;
+        }
+    }
+
+    void stopAnimation()
+    {
+        foreach (AnimationState state in anim)
+        {
+            state.speed = 0F;
+        }
+
+    }
+    
+}
